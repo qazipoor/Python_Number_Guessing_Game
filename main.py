b45998bd4a213a3235d1bdfb19134fec56626eb0
Ali Qazipoor <qazipoor@gmail.com>
2025-10-03 21:53:18 +0300

    Init

---
+++ main.py
@@ -0,0 +1,11 @@
+# Choosing a random number between 1 and 100
+
+# Function to set difficulty
+
+# Let the user guess a number
+
+# Function to check users' guess against actual answer
+
+# Track the number of turns and reduce by 1 if they get it wrong
+
+# Repeat the guessing functionality if they get it wrong.
